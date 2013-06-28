d71d1cbeee1e867c2c26bf752aec4f0f097381ed
Sergey Ovechkin <me@sovechkin.com>
2013-06-28 13:05:40 +0400

    обновлён capistrano;npm install на сервере;смена порта node.js при деплое

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,17 +2,40 @@ set :application, "moonu.ru"
 #========================
 #CONFIG
 #========================
-require "capistrano-offroad"
-offroad_modules "defaults", "supervisord"
+require           "capistrano-offroad"
+offroad_modules   "defaults", "supervisord"
 
-set :repository,  "git@github.com:pomeo/moonu.ru.git"
-set :user, "pomeo"
-set :port, 2222
-set :use_sudo, false
-set :deploy_via, :copy
-set :scm, :git
+default_run_options[:pty] = true
+default_run_options[:shell] = '/bin/bash'
+set :repository,  "git@github.com:pomeo/#{application}.git"
+set :user,        "pomeo"
+set :port,        2222
+set :use_sudo,    false
+set :deploy_via,  :copy
+set :scm,         :git
+set :copy_exclude,["/.git/", "/.gitignore", "/Capfile", "/config/", "/config.yaml", "/Rakefile", "Rules", "/tmp/", "/mkmf.log"]
+
+set :supervisord_pidfile, "/var/run/supervisord.pid"
+set :supervisord_start_group, "moonu"
+set :supervisord_stop_group, "moonu"
 #========================
 #ROLES
 #========================
-role :app, "#{application}"
-set :deploy_to, "/var/www/#{application}/www"
+role :app,        "#{application}"
+set :deploy_to,   "/var/www/#{application}/www"
+set :deploy_user, "pomeo"
+set :deploy_group,"pomeo"
+
+namespace :deploy do 
+  desc "Install node modules non-globally"
+  task :npm_install do
+    run ". /home/pomeo/.nvm/nvm.sh && cd #{current_path} && npm install"
+  end
+
+  desc "Change node.js port"
+  task :chg_port do
+    run "sed -i 's/3000/3050/g' #{current_path}/app.js"
+  end
+end
+ 
+after "deploy:update_code", "deploy:npm_install", "deploy:chg_port", "deploy:restart"
